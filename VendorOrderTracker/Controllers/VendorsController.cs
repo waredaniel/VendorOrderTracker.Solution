1819569e23724a240d1834269ccd2e5c4964f078
danielware <waredanielb@gmail.com>
2022-07-22 12:15:27 -0700

    Create working form and display for inputted Vendors.

--- VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -8,18 +8,24 @@ namespace VendorOrderTracker.Controllers
     public class VendorsController : Controller
   {
 
-    // [HttpGet("/vendors")]
-    // public ActionResult Index()
-    // {
-    //   List<Vendor> allVendors = Vendor.GetAll();
-    //   return View(allVendors);
-    // }
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
 
    [HttpPost("/vendors")]
     public ActionResult Create(string vendorName)
     {
       Vendor newVendor = new Vendor(vendorName);
       return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
     }
 }
 }
